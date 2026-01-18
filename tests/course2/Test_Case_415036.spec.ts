3858d6704340dfcc80216f4b8b320498dbff0c5b
Efrem Yohanis Anbese <126686114+Efrem-Yohanis@users.noreply.github.com>
2026-01-18 06:51:25 -0800

    Update Test_Case_415036.spec.ts

--- tests/course2/Test_Case_415036.spec.ts
+++ tests/course2/Test_Case_415036.spec.ts
@@ -72,11 +72,10 @@ await page.getByLabel('Depositor CIK*').fill(dealData.depositorCIK);
 await page.getByLabel('Sponsor CIK*').fill(dealData.sponsorCIK);
 
 // ---------------- Asset Class ----------------
-await page.getByLabel('Asset Class*').click();
-await page.getByRole('option', {
-  name: dealData.assetClass,
-  exact: true
-}).click();
+const assetClassInput = page.locator('input[id^="react-select"][id$="-input"]');
+await assetClassInput.click();
+await assetClassInput.fill(dealData.assetClass);
+await assetClassInput.press('Enter');
 
 // ---------------- ABS-EE Period Dates ----------------
 await page.getByLabel('ABS-EE Start Period*').fill(dealData.absPeriodStart);
@@ -91,9 +90,4 @@ await page.getByRole('button', { name: 'Save' }).click();
 // ---------------- Assertions ----------------
 await expect(page.getByText('Saved successfully')).toBeVisible();
 await expect(page.getByText('Success', { exact: true })).toBeVisible();
-
 });
-
-=======================
-  <div class=" css-1hwfws3"><div class="css-1rhbuit-multiValue"><div class="css-12jo7m5">Auto loans</div><div class="css-xb97g8"><svg height="14" width="14" viewBox="0 0 20 20" aria-hidden="true" focusable="false" class="css-19bqh2r"><path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z"></path></svg></div></div><div class="css-1g6gooi"><div class="" style="display: inline-block;"><input autocapitalize="none" autocomplete="off" autocorrect="off" id="react-select-129-input" spellcheck="false" tabindex="0" type="text" aria-autocomplete="list" value="" style="box-sizing: content-box; width: 2px; background: 0px center; border: 0px; font-size: inherit; opacity: 1; outline: 0px; padding: 0px; color: inherit;"><div style="position: absolute; top: 0px; left: 0px; visibility: hidden; height: 0px; overflow: scroll; white-space: pre; font-size: 16px; font-family: -apple-system, BlinkMacSystemFont, &quot;Segoe UI&quot;, Roboto, &quot;Helvetica Neue&quot;, Arial, &quot;Noto Sans&quot;, sans-serif, &quot;Apple Color Emoji&quot;, &quot;Segoe UI Emoji&quot;, &quot;Segoe UI Symbol&quot;, &quot;Noto Color Emoji&quot;; font-weight: 400; font-style: normal; letter-spacing: normal; text-transform: none;"></div></div></div></div>
-                                              
