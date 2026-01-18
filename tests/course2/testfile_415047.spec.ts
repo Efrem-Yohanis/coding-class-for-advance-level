557ea207ba543b27f8a6896bbc653f48cae3677e
Efrem Yohanis Anbese <126686114+Efrem-Yohanis@users.noreply.github.com>
2026-01-18 09:16:35 -0800

    Update dealData key for test case 415036

--- tests/course2/testfile_415047.spec.ts
+++ tests/course2/testfile_415047.spec.ts
@@ -10,7 +10,7 @@ const rawData = fs.readFileSync(testDataPath, 'utf-8');
 const testData = JSON.parse(rawData);
 
 // Use the key for this specific test
-const dealData = testData['415036'];
+const dealData = testData['415047'];
 
 test('415036 - Create and upload ABSEE deal', async ({ page }) => {
   // --- FILE PATH ---
